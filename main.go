47dcb018ebc233b30dd66d3c866cbe289e5a8fb5
zxc <15660589213@163.com>
2023-04-24 20:22:45 +0800

    file_pipeline:init

--- main.go
+++ main.go
@@ -1,32 +1,22 @@
 package main
 
 import (
-	"github.com/zserge/lorca"
-	"log"
-	"os"
-	"os/signal"
-	"syscall"
+	"github.com/gin-gonic/gin"
+	"os/exec"
 )
 
 func main() {
-	//var ui lorca.UI
-	//--disable-sync 取消同步功能
-	//--disable-translate 取消翻译功能
-	ui, err := lorca.New("https://www.baidu.com", "", 800, 600, "--disable-sync", "--disable-translate")
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-	// 监听 中断信号
-	chSignal := make(chan os.Signal, 1)
-	// Notify 订阅 监听到中断信号
-	//syscall.SIGINT 中断信号
-	//syscall.SIGTERM 终止信号
-	signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM)
-
-	select {
-	case <-ui.Done():
-	case <-chSignal:
-	}
-
-	ui.Close()
+	go func() {
+		//gin.SetMode()
+		r := gin.Default()
+		r.GET("/", func(context *gin.Context) {
+			context.String(200, "hello")
+		})
+		r.Run(":8080")
+	}()
+	//打开chrome 窗口
+	chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+	cmd := exec.Command(chromePath, "--app=http://localhost:8080", "--window-size 400 400")
+	cmd.Start()
+	select {}
 }
