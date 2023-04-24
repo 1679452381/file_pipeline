a42eb1e3fe29cab5bc0c9345ac1165a9b208849a
zxc <15660589213@163.com>
2023-04-24 21:18:38 +0800

    file_pipeline:静态文件

--- main.go
+++ main.go
@@ -1,25 +1,58 @@
 package main
 
 import (
+	"embed"
 	"github.com/gin-gonic/gin"
+	"io/fs"
+	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"os/signal"
+	"strings"
 )
+
+//embed.FS:用于嵌入文件,如通过go:embed嵌入本地文件
+
+//go:embed frontend/dist/*
+var FS embed.FS
 
 func main() {
 	go func() {
 		//gin.SetMode()
 		r := gin.Default()
-		r.GET("/", func(context *gin.Context) {
-			context.String(200, "hello")
+		//r.GET("/", func(context *gin.Context) {
+		//	context.String(200, "hello")
+		//})
+
+		// fs.Sub会从f(embed.FS)中提取所有前缀为"frontend/dist"的文件,并返回一个新的FS,包含这棵子树。
+		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+		r.StaticFS("/static", http.FS(staticFiles))
+		// 静态文件不存在 渲染html
+		r.NoRoute(func(c *gin.Context) {
+			path := c.Request.URL.Path
+			if strings.HasPrefix(path, "/static/") {
+				reader, err := staticFiles.Open("index.html")
+				if err != nil {
+					log.Fatalln(err)
+				}
+				defer reader.Close()
+				stat, err := reader.Stat()
+				if err != nil {
+					log.Fatalln(err)
+				}
+				c.DataFromReader(http.StatusOK, stat.Size(), "text/html", reader, nil)
+			} else {
+				c.Status(http.StatusNotFound)
+			}
 		})
+
 		r.Run(":8080")
 	}()
 
 	//打开chrome 窗口下`
 	chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-	cmd := exec.Command(chromePath, "--app=http://localhost:8080/")
+	cmd := exec.Command(chromePath, "--app=http://localhost:8080/static/index.html")
 	cmd.Start()
 
 	//监听退出序号
