888efda65de31c6e87d264254c768bba8889626f
zxc <15660589213@163.com>
2023-04-26 01:01:55 +0800

    file_pipeline:chan 监听窗口退出

--- main.go
+++ main.go
@@ -17,26 +17,34 @@ func main() {
 	}()
 
 	chanChrome := make(chan struct{})
+	chanBackendDie := make(chan struct{})
 	//cmd打开chrome窗口
-	go CmdStartChrome(chanChrome)
-	fmt.Println("关闭了")
+	go CmdStartChrome(chanChrome, chanBackendDie)
 
 	//监听退出序号
 	chanSignal := InterruptSignal()
 	//select 会一直轮询直到chanSignal 有值
-	select {
-	case <-chanSignal:
-	case <-chanChrome:
-		os.Exit(0)
-		return
+	for {
+		select {
+		case <-chanSignal:
+			fmt.Println("esc")
+			chanBackendDie <- struct{}{}
+		case <-chanChrome:
+			os.Exit(0)
+			return
+		}
 	}
 }
 
 // cmd
-func CmdStartChrome(chanChrome chan struct{}) *exec.Cmd {
+func CmdStartChrome(chanChrome, chanBackendDie chan struct{}) *exec.Cmd {
 	chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
 	cmd := exec.Command(chromePath, "--app=http://localhost:"+define.Port+"/static/index.html")
 	cmd.Start()
+	go func() {
+		<-chanBackendDie
+		cmd.Process.Kill()
+	}()
 	cmd.Wait()
 	chanChrome <- struct{}{}
 	return cmd
